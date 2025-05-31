bae3584ff8070aec08f9e92fb47f0dc6b5473475
melcolmm <31358679+melcolmm@users.noreply.github.com>
2025-05-31 12:25:03 +0200

    Added initial numerical button code

--- script.js
+++ script.js
@@ -15,15 +15,84 @@ const btnMinus = document.querySelector("#btnMinus");
 const btnMultiply = document.querySelector("#btnMultiply");
 const btnDivide = document.querySelector("#btnDivide");
 const btnEqual = document.querySelector("#btnEqual");
+const disp = document.querySelector("#display");
 
 let num1 = "";
 let num2 = "";
 let operator = "";
-
-
-
+let operatorPressed = false;
 
 operate(6,2,"+");
+
+btnOne.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "1";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnTwo.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "2";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnThree.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "3";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnFour.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "4";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnFive.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "5";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnSix.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "6";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnSeven.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "7";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnEight.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "8";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnNine.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "9";
+        console.log("num 1 = " + num1); 
+    } 
+});
+
+btnZero.addEventListener('click', () => {
+    if (operatorPressed == false) {
+        num1 = num1 + "0";
+        console.log("num 1 = " + num1); 
+    } 
+});
 
 function add(num1, num2) {
     result = parseInt(num1) + parseInt(num2);
@@ -65,3 +134,4 @@ function operate(num1, num2, operator) {
             break;
     }
 }
+
